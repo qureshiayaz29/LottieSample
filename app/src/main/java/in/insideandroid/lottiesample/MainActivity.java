6b0d70e45882fbb690b2d3ce0a573046371d438a
Ayaz Qureshi <ayaz_cont2@olaelectric.in>
2020-08-11 13:38:47 +0530

    first commit

---
+++ app/src/main/java/in/insideandroid/lottiesample/MainActivity.java
@@ -0,0 +1,19 @@
+package in.insideandroid.lottiesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
+import com.airbnb.lottie.LottieOnCompositionLoadedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
